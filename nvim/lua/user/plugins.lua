e077a27bef08b47eba3afe6186dab5cd9cfc158e
Cesar Palacios <cesarlp.ng@gmail.com>
2022-06-26 17:05:16 -0300

    add coc

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -75,6 +75,7 @@ return packer.startup(function(use)
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
+  use {'neoclide/coc.nvim', branch = 'release'}
   -- TODO: figure out how to use project formatter config
   -- use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
 
