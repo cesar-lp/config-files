b921998a31cb77a3757aadd3863066655d9acba7
Cesar Palacios <cesarlp.ng@gmail.com>
2023-04-06 16:47:32 -0300

    telescope: improve search ui

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -7,15 +7,7 @@ end
 local actions = require "telescope.actions"
 
 telescope.setup {
-  extensions = {
-    fuzzy = true,                    -- false will only do exact matching
-    override_generic_sorter = true,  -- override the generic sorter
-    override_file_sorter = true,     -- override the file sorter
-    case_mode = "smart_case",        -- or "ignore_case" or "respect_case", the default case_mode is "smart_case"
-  },
-
   defaults = {
-
     file_ignore_patterns = {
       "node_modules",
       "build",
@@ -24,7 +16,20 @@ telescope.setup {
 
     prompt_prefix = " ",
     selection_caret = " ",
-    path_display = { "smart" },
+    path_display = { "truncate" },
+    sorting_strategy = "ascending",
+    layout_config = {
+      horizontal = {
+        prompt_position = "top",
+        preview_width = 0.55,
+      },
+      vertical = {
+        mirror = false,
+      },
+      width = 0.87,
+      height = 0.80,
+      preview_cutoff = 120,
+    },
 
     mappings = {
       i = {
